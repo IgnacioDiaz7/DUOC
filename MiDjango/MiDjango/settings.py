76c4b2c957939116fb9016c17ee86f175198ce95
IgnacioDiaz7 <ign.diazv@duocuc.cl>
2023-06-30 18:29:38 -0400

    conexion a bbdd lista

--- MiDjango/MiDjango/settings.py
+++ MiDjango/MiDjango/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'MiDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/ORCL',
+        'USER': 'Nacho',
+        'PASSWORD': 'prueba',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
